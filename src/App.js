923230212a2ffc56235b1bfe4cbcd388c58be7b2
shafiuzzama <shafiuzzama02@gmail.com>
2024-03-29 12:08:29 +0530

    register form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Register from './components/Register';
 
 function App() {
   return (
     <div >
-<h1>CourseManagement</h1>
+  <Register/>
     </div>
   );
 }
