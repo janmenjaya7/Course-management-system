9c6b81fcdf6619ea4a67ddf0935b84e7715a4c03
janmenjaya <janmenjayabiswal@gmail.com>
2024-03-30 10:50:23 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
-import LandingPages from "./components/LandingPage/LandingPages";
-import Navbar from "./components/Navbar/Navbar";
-import CardSection from "./components/card/CardSection";
+import Routing from "./Router/Router";
+// import LandingPages from "./components/LandingPage/LandingPages";
+// import Navbar from "./components/Navbar/Navbar";
+// import CardSection from "./components/card/CardSection";
+
 function App() {
   return (
     <>
-      <Navbar />
+      <Routing />
+      {/* <Navbar />
       <CardSection />
-      <LandingPages />
+      <LandingPages /> */}
     </>
   );
 }
