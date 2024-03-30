1c963692d41f6ebb783d9ba7ecdd84d3517829e0
Sunil Gowda <sunilcgowda8055@gmail.com>
2024-03-30 11:26:56 +0530

    main updated

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import "./App.css";
-<<<<<<< HEAD
-import Routing from "./Router/Router";
-// import LandingPages from "./components/LandingPage/LandingPages";
-// import Navbar from "./components/Navbar/Navbar";
-// import CardSection from "./components/card/CardSection";
-
-=======
-import LandingPages from "./components/LandingPage/LandingPages";
-import Navbar from "./components/Navbar/Navbar";
-import CardSection from "./components/card/CardSection";
-import Login from "./components/register-login/Login ";
-import Register from "./components/register-login/Register";
-import Searchnav from "./components/searchnvabar/Searchnav";
->>>>>>> 2a4f4ac86e046a3606064be7c2315eef3a0fbb5b
+import './App.css'
+import LandingPages from './components/LandingPage/LandingPages'
+import Navbar from './components/Navbar/Navbar'
+import CardSection from './components/card/CardSection'
+import Login from './components/register-login/Login '
+import Register from './components/register-login/Register'
+import Routing from './Router/Router.jsx'
+import Searchnav from './components/searchnvabar/Searchnav'
 function App() {
   return (
     <>
       <Routing />
       {/* <Navbar />
       <CardSection />
-<<<<<<< HEAD
       <LandingPages /> */}
-=======
       <LandingPages />
-      <Register heading="Start learning" title="Register Now"  />
+      <Register heading="Start learning" title="Register Now" />
       <Login heading="Welcome back" title="Login" />
-      <Searchnav/>
->>>>>>> 2a4f4ac86e046a3606064be7c2315eef3a0fbb5b
+      <Searchnav />
     </>
-  );
+  )
 }
-export default App;
+export default App
