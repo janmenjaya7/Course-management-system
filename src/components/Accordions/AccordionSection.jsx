c5d18545fe3e2a3b4e917c9c8fe8ab0a590b3c7b
Sunil Gowda <sunilcgowda8055@gmail.com>
2024-03-30 10:59:04 +0530

    button update

--- src/components/Accordions/AccordionSection.jsx
+++ src/components/Accordions/AccordionSection.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+
+
+import React from 'react';
+import './AccordionSection.css';
 import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
   Typography,
-} from '@mui/material'
-// import AddIcon from '@mui/icons-material/Add'
+} from '@mui/material';
 
-function AccordionSection() {
+
+function AccordionSection({ id, onClick }) {
   return (
     <div>
-      <Accordion>
+      <Accordion id={id} className="custom-accordion">
         <AccordionSummary
-          expandIcon={<>Icon</>}
+          className="accordion-summary"
+          expandIcon={<span>Icon</span>}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
           <Typography>Accordion 1</Typography>
         </AccordionSummary>
-        <AccordionDetails>
+        <AccordionDetails className="accordion-details">
           <Typography>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
@@ -27,7 +31,7 @@ function AccordionSection() {
         </AccordionDetails>
       </Accordion>
     </div>
-  )
+  );
 }
 
-export default AccordionSection
+export default AccordionSection;
--- src/components/buttons/ButtonSection.jsx
+++ src/components/buttons/ButtonSection.jsx
@@ -1,40 +1,24 @@
-<<<<<<< HEAD
-import React from 'react';
-import Button from '@mui/material/Button';
-import './ButtonSection.css';
+import React from 'react'
+import Button from '@mui/material/Button'
+import './ButtonSection.css'
 
 const ButtonSection = ({ id, variant, onClick, buttonText }) => {
-  let buttonClass = 'custom-button';// DEFAULT CLASS FOR ALL VARIANTS.
+  let buttonClass = 'custom-button' // DEFAULT CLASS FOR ALL VARIANTS.
 
   // CLASS BASED ON VARIANTS.
   if (variant === 'outlined') {
-    buttonClass += ' outlined';
+    buttonClass += ' outlined'
   } else if (variant === 'text') {
-    buttonClass += ' text';
-  }
-  else if(variant==="contained"){
-    buttonClass += 'contained'
-
+    buttonClass += ' text'
+  } else if (variant === 'contained') {
+    buttonClass += ' contained'
   }
 
   return (
     <Button id={id} variant={variant} onClick={onClick} className={buttonClass}>
       {buttonText}
     </Button>
-  );
-};
-=======
-import React from "react";
-import Button from "@mui/material/Button";
-function ButtonSection({ variant, handleClick, btnTitle }) {
-  return (
-    <div>
-      <Button variant={variant} onClick={handleClick}>
-        {btnTitle}
-      </Button>
-    </div>
-  );
+  )
 }
->>>>>>> 9652158f408d67e2887cdf06e8c033263ac35d42
 
-export default ButtonSection;
+export default ButtonSection
