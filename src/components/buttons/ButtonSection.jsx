7c8e5099337dd879610349af609599e16e339026
Sunil Gowda <sunilcgowda8055@gmail.com>
2024-04-04 20:21:56 +0530

    courses page with responsive

--- src/components/buttons/ButtonSection.jsx
+++ src/components/buttons/ButtonSection.jsx
@@ -1,36 +1,24 @@
-import React from 'react'
-import Button from '@mui/material/Button'
-import './ButtonSection.css'
-import React from "react";
-import Button from "@mui/material/Button";
-import "./ButtonSection.css";
+import React from 'react';
+import Button from '@mui/material/Button';
+import './ButtonSection.css';
 
 const ButtonSection = ({ id, variant, onClick, buttonText }) => {
-  let buttonClass = 'custom-button' // DEFAULT CLASS FOR ALL VARIANTS.
-  let buttonClass = "custom-button"; // DEFAULT CLASS FOR ALL VARIANTS.
+  let buttonClass = 'custom-button'; // DEFAULT CLASS FOR ALL VARIANTS.
 
   // CLASS BASED ON VARIANTS.
   if (variant === 'outlined') {
-    buttonClass += ' outlined'
+    buttonClass += ' outlined';
   } else if (variant === 'text') {
-    buttonClass += ' text'
+    buttonClass += ' text';
   } else if (variant === 'contained') {
-    buttonClass += ' contained'
-  if (variant === "outlined") {
-    buttonClass += " outlined";
-  } else if (variant === "text") {
-    buttonClass += " text";
-  } else if (variant === "contained") {
-    buttonClass += "contained";
+    buttonClass += ' contained';
   }
 
   return (
     <Button id={id} variant={variant} onClick={onClick} className={buttonClass}>
       {buttonText}
     </Button>
-  )
-}
   );
 };
 
-export default ButtonSection
+export default ButtonSection;
