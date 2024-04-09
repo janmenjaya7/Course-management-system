16fc478526b8ca247c34bdf62cbc226907ae8ffb
Sunil Gowda <sunilcgowda8055@gmail.com>
2024-04-09 14:39:11 +0530

    search nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Router from "./Router/Router";
+import Searchnav from "./components/searchnvabar/Searchnav";
 function App() {
   return (
     <>
-      <Router />
+      {/* <Router /> */}
+      <Searchnav/>
     </>
   )
 }
