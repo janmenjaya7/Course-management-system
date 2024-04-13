d9b81c9af75cf2dab4051e2c4ece7d3d8ac4f2d5
janmenjaya <janmenjayabiswal@gmail.com>
2024-04-13 19:33:04 +0530

    addCard

--- src/components/register-login/Register.jsx
+++ src/components/register-login/Register.jsx
@@ -5,17 +5,7 @@ import passwordicon from "../../assets/passwordicon/eye.svg";
 import Input from "../inpute/Input";
 import { useNavigate } from "react-router-dom";
 
-
 const Register = ({ heding, title }) => {
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [nameEroor, setNameError] = useState("");
-  // const [emailError, setEmailError] = useState("");
-  // const [passwordError, setPasswordError] = useState("");
-  // const [passwordVisible, setPasswordVisible] = useState(false);
-
-  // const [userData, setUserData] = useState([]);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -23,24 +13,20 @@ const Register = ({ heding, title }) => {
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
-
-  const [userData, setUserData] = useState([]);
-
   const navigate = useNavigate();
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+    setNameError("");
+  };
 
-  const handleNameChange=(event)=>{
-  setName(event.target.value);
-  setNameError("")
-   }
-  
-   const handleEmailChange=(event)=>{
-  setEmail(event.target.value)
-  setEmailError("")
-   }
-   const handlePasswordChnage=(event)=>{
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    setEmailError("");
+  };
+  const handlePasswordChnage = (event) => {
     setPassword(event.target.value);
     setPasswordError("");
-   }
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -75,20 +61,19 @@ const Register = ({ heding, title }) => {
       const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
       // existingUsers.push(data);
       // console.log("existingUsers", existingUsers);
-     
+
       const isUserExist = existingUsers.filter((user) => user.email === email);
       console.log("isUserExist", isUserExist);
-      if (isUserExist.length>=1 ) {
+      if (isUserExist.length >= 1) {
         alert("you already register");
       } else {
         // existingUsers.push(data);
         // localStorage.setItem("userData", JSON.stringify([...existingUsers,data]));
         // navigate("/courses");
 
-        localStorage.setItem('users', JSON.stringify([...existingUsers, data]));
+        localStorage.setItem("users", JSON.stringify([...existingUsers, data]));
         navigate("/courses");
       }
-  
     }
   };
 
@@ -96,7 +81,6 @@ const Register = ({ heding, title }) => {
     setPasswordVisible(!passwordVisible);
   };
   return (
-  
     <div>
       <div className="outersection">
         <div className="container-box">
@@ -104,53 +88,50 @@ const Register = ({ heding, title }) => {
             <h1 className="heading">{heding}</h1>
             <h4 className="Register-Now">{title}</h4>
             <div className=" inner-input">
-
-            <Input 
-              type={"text"}
-              label={"Full name"}
-              placeholder={"Full name"}
-              handleChange={handleNameChange}
-              value={name}
-            />
-            <div className="nameEroor">{nameEroor}</div>
+              <Input
+                type={"text"}
+                label={"Full name"}
+                placeholder={"Full name"}
+                handleChange={handleNameChange}
+                value={name}
+              />
+              <div className="nameEroor">{nameEroor}</div>
             </div>
             <div className=" inner-input">
-
-            {/* <Input 
+              {/* <Input 
             <div className="nameEroor">{nameEroor}</div>
             </div>
             <div className=" inner-input"> */}
 
-            <Input 
-              type={"email"}
-              label={"Email id"}
-              placeholder={"Email"}
-              handleChange={handleEmailChange}
-              value={email}
-            />
-            <div className="emailError">{emailError}</div>
+              <Input
+                type={"email"}
+                label={"Email id"}
+                placeholder={"Email"}
+                handleChange={handleEmailChange}
+                value={email}
+              />
+              <div className="emailError">{emailError}</div>
             </div>
             <div className=" inner-input">
-
-            <div className="password-input">
-              <Input 
-                type={passwordVisible ? "text" : "password"}
-                label={"Password"}
-                placeholder={"Password"}
-                handleChange={handlePasswordChnage}
-                value={password}
-              />
-
-              {
-                <img
-                  className="img-section"
-                  src={passwordicon}
-                  alt="icon"
-                  onClick={togglePasswordVisibility}
+              <div className="password-input">
+                <Input
+                  type={passwordVisible ? "text" : "password"}
+                  label={"Password"}
+                  placeholder={"Password"}
+                  handleChange={handlePasswordChnage}
+                  value={password}
                 />
-              }
-            </div>
-            <div className="passwordError">{passwordError}</div>
+
+                {
+                  <img
+                    className="img-section"
+                    src={passwordicon}
+                    alt="icon"
+                    onClick={togglePasswordVisibility}
+                  />
+                }
+              </div>
+              <div className="passwordError">{passwordError}</div>
             </div>
             {/* <button type="submit" className="custom-button"> */}
             <button type="submit" className="custom-button">
@@ -164,8 +145,6 @@ const Register = ({ heding, title }) => {
         </div>
       </div>
       <div className="login-link">
-        Already have an account?
-        <span onClick={() => navigate("/login")}>Login</span>
         Already have an account?
         <span onClick={() => navigate("/login")}>Login</span>
       </div>
